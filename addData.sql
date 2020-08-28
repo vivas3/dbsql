a0f53000fb4930ccd1d756830d6ca0484321f67b
Visa <visa.saarinen@gmail.com>
2020-08-28 19:19:43 +0300

    loppu

--- addData.sql
+++ addData.sql
@@ -1034,3 +1034,147 @@ UPDATE Tentti
 SET ilmoittautuneita = ilmoittautuneita+1
 WHERE kurssikoodi = 'MAT-1000' AND  tenttiAika = '2020-10-09 09:00:00';
 
+INSERT INTO Varaus 
+VALUES ((SELECT MAX(varausNumero)+1 FROM Varaus),'Harjoitus','2020-09-21 12:00:00',DATETIME('2020-09-21 12:00:00','03:00'),'K1');
+
+INSERT INTO VarattuHarjoitusRyhmälle 
+VALUES ((SELECT MAX(varausNumero) FROM Varaus),'2020-09-21 12:00:00','K101');
+
+INSERT INTO Varaus 
+VALUES ((SELECT MAX(varausNumero)+1 FROM Varaus),'Harjoitus','2020-08-15 12:00:00',DATETIME('2020-08-15 12:00:00','03:00'),'K1');
+
+INSERT INTO VarattuHarjoitusRyhmälle 
+VALUES ((SELECT MAX(varausNumero) FROM Varaus),'2020-08-15 12:00:00','K101');
+
+-- Luodaan näkymä LuentoSali jotta luennoitsijat voivat etsiä kätevämmin suurempia luentosaleja
+CREATE VIEW LuentoSali AS
+   SELECT saliID
+   FROM Sali
+   WHERE paikkoja >= 100
+
+-- Usein esimerkiksi luennoitsijat ovat kiinnostuneita yhden tietyn salin varauksista. Koska ‘Varaus’ -taulu on yksi suurimmista tauluista, sen kokonaan läpi käymiseen kuluu paljon aikaa. Kun kyseiseen tauluun tekee hakemiston atribuutin saliID mukaan, yhteen saliin liittyvien varausten etsimiseen kulunut aika lyhenee. 
+CREATE INDEX SaliIndeksi ON Varaus(saliID);
+
+-- Haetaan kaikki harjoitusryhmät tietyssä opintojaksossa (esim. ensimmäisessä), joissa on vielä tilaa
+SELECT harjoitusRyhmä FROM Harjoitus
+WHERE ilmoittautuneita<maxOsallistujat
+AND opintojakso=1;
+
+-- Haetaan kaikki opintojaksot halutulta lukukaudelta, joissa tietty kurssi järjestetään
+SELECT opintojakso
+FROM Toteutuskerta
+WHERE kurssikoodi = 'MS-A0002' AND lukukausi = '2020-2021';
+
+-- Opiskelija haluaa vielä yhden kurssin lukuvuoden ensimmäiseen opintojaksoon. Haetaan kaikkien kurssien kurssikoodit, jotka pidetään lukukauden ‘2020-2021’ ensimmäisessä opintojaksossa
+SELECT kurssikoodi
+FROM Toteutuskerta
+WHERE lukukausi = '2020-2021' AND opintojakso = 1;
+
+-- Opiskelija löytää kurssin MS-A0001 ja haluaa löytää siihen liittyvät harjoitusryhmät joissa on vielä tilaa
+SELECT *
+FROM Harjoitus
+WHERE kurssikoodi = 'MS-A0001' AND lukukausi = '2020-2021' AND opintojakso = 1 AND maxOsallistujat > ilmoittautuneita;
+
+-- Luennoitsija haluaa luennolleen salin johon mahtuu ainakin 100 ihmistä, sekä joka on vapaana 2020-09-01 12:00 - 2020-09-01 14:00. Haetaan kaikista saleista ne joissa on paikkoja 100 ja niihin liittyvien varausten loppuaika on halutun aikavälin aloitusaikaa pienempi, tai alkuaika lopetusaikaa suurempi
+SELECT LuentoSali.saliID
+FROM LuentoSali LEFT OUTER JOIN Varaus ON LuentoSali.saliID = Varaus.saliID
+WHERE (varausNumero IS NULL OR loppuaika <= '2020-09-01 12:00' OR alkuAika >= '2020-09-01 14:00')
+-- varausNumero IS NULL -vaihtoehdolla saamme myös ne salit joilla ei ole yhtään varausta
+-- Tämän jälkeen luennoitsija voi valita mieluisensa salin ja tehdä siihen tarvittavat varaukset
+
+-- Haetaan kaikki “Mat” tutkinto-ohjelman opiskelijat, jotka ovat ilmoittautuneet johonkin lukukauden 2020-2021 kurssille (eli harjoitusryhmään)
+SELECT DISTINCT nimi
+FROM Opiskelija, IlmoittautunutRyhmään AS I, Harjoitus
+WHERE Opiskelija.opiskelijaNumero = I.opiskelijaNumero
+    AND I.harjoitusRyhmä = Harjoitus.harjoitusRyhmä
+    AND lukukausi = '2020-2021' AND tutkintoOhjelma = 'Mat';
+
+-- Haetaan sali, johon mahtuu yli 80 opiskelijaa ja jossa on videotallennusmahdollisuus
+SELECT S.saliID FROM Sali S, SisältääVarusteen SV
+WHERE S.saliID=SV.saliID AND paikkoja>80
+AND varustetunniste='Videotallennus';
+
+-- Haetaan sali, johon mahtuu yli 80 opiskelijaa ja jossa on videotallennusmahdollisuus
+SELECT S.saliID FROM Sali S, SisältääVarusteen SV
+WHERE S.saliID=SV.saliID AND paikkoja>80
+AND varustetunniste='Videotallennus';
+
+-- Haetaan sali, jossa on vähintään 5 tietokonetta ja mahdollisimman monta paikkaa
+SELECT S.saliID, paikkoja
+FROM SisältääVarusteen AS S, Sali
+WHERE S.saliID = Sali.saliID AND varusteTunniste = 'Tietokone' AND lukumäärä >= 5
+ORDER BY paikkoja DESC;
+
+-- Halutaan selvittää sen rakennuksen nimi ja osoite jossa salin T1 sijaitsee 
+SELECT nimi, osoite
+FROM Sali, Rakennus
+WHERE Sali.rakennuksenNimi = Rakennus.nimi AND Sali.saliID = 'T1';
+
+-- Halutaan selvittää kaikkien opiskelijoiden nimet ja opiskelijanumerot, jotka ovat ilmoittautuneet harjoitusryhmään M101
+SELECT nimi, I.opiskelijaNumero
+FROM Opiskelija, IlmoittautunutRyhmään AS I
+WHERE Opiskelija.opiskelijaNumero = I.opiskelijaNumero AND I.HarjoitusRyhmä = 'M101';
+
+-- Halutaan selvittää kaikki harjoitusryhmän M101 kokoontumisajat
+SELECT kokoontumisAika
+FROM HarjoitusAika
+WHERE HarjoitusRyhmä = 'M101';
+
+-- Opiskelija haluaa tietää, missä salissa hänen tenttinsä järjestetään
+SELECT saliID
+FROM VarattuTentille, Varaus
+WHERE VarattuTentille.varausNumero = Varaus.varausNumero AND kurssikoodi = 'MS-A0001' AND tenttiAika = '2020-09-01 14:15:00';
+
+-- Kurssivastaava haluaa selvittää mihin kyseisen harjoitusryhmän tapaamiskertoihin ei olla vielä tehty tilavarausta. Kyseiselle harjoitusryhmälle mahtuu maksimissaan 100 opiskelijaa, joten halutaan tehdä varaus siten, että sinne mahtuu ainakin kaikki mutta ei haluta varata esim. 500 hengen luentosalia vain sadalle hengelle.
+SELECT kokoontumisAika ALKAA,
+datetime(kokoontumisAika,harjoituksenKesto) LOPPUU
+FROM Harjoitus H, HarjoitusAika HA
+WHERE H.harjoitusRyhmä=HA.harjoitusRyhmä
+AND H.harjoitusRyhmä='K101'
+EXCEPT
+SELECT alkuAika ALKAA, loppuAika LOPPUU
+FROM Varaus V, VarattuHarjoitusRyhmälle VHR
+WHERE V.varausNumero=VHR.varausNumero
+AND harjoitusRyhmä='K101') as ajat,
+(SELECT saliID SALI FROM Sali
+WHERE paikkoja>=100 AND paikkoja<=200
+-- Tällä saadaan kaikki mahdolliset halutut reunaehdot täyttävät vaihtoehdot saleille. Kysely ei kuitenkaan kerro vielä onko nämä salit vapaita haluttuina aikoina. Tämän jälkeen täytyisi vain katsoa käyttöliittymästä erikseen kullekin salille vapaa aika.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
